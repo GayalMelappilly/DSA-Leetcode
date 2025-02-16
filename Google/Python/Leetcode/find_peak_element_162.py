889a86c17be269a91afaa04ae2165519b0c50e35
Gayal Melappilly <gayalsunil@gmail.com>
2025-02-16 15:47:51 +0530

    Update: Find peak element

--- Google/Python/Leetcode/find_peak_element_162.py
+++ Google/Python/Leetcode/find_peak_element_162.py
@@ -3,12 +3,26 @@ class Solution:
 
         leng = len(nums)
 
-        for index,num in enumerate(nums):
-            if index != 0 and index != leng-1:
-                if num>nums[index-1] and num>nums[index+1]:
-                    return index
-        
-        if nums[0]>nums[leng-1]:
+        if leng == 1:
             return 0
-        else:
-            return leng-1    
+        
+        if leng == 2:
+            if nums[0]<nums[1]:
+                return 1
+            return 0
+
+        l, r = 0, leng-1
+
+        if nums[l]>nums[l+1]:
+            return l
+        if nums[r]>nums[r-1]:
+            return r
+
+        while l<r:
+            mid = (l+r)//2
+            if nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1]:
+                return mid
+            if nums[mid]<nums[mid+1]:
+                l = mid
+            elif nums[mid]<nums[mid-1]:
+                r = mid 
